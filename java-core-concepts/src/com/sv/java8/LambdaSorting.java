b50fd8fd26d2e1d6362bb7e56776212c6c9a237b
venkatesh.sajjanapu <venkatesh.sajjanapu.contractor@bbva.com>
2018-04-24 12:35:51 -0500

    changes

---
+++ java-core-concepts/src/com/sv/java8/LambdaSorting.java
@@ -0,0 +1,58 @@
+package com.sv.java8;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * ref: https://www.mkyong.com/java8/java-8-lambda-comparator-example/
+ * 
+ * @author Venkat
+ *
+ */
+public class LambdaSorting {
+
+	public static void main(String[] args) {
+
+		List<Developer> listDevs = getDevelopers();
+
+		System.out.println("Before Sort");
+		for (Developer developer : listDevs) {
+			System.out.println(developer);
+		}
+
+		System.out.println("After Sort");
+
+		// lambda here!
+		listDevs.sort((Developer o1, Developer o2) -> o1.getAge() - o2.getAge());
+
+		// lambda, valid, parameter type is optional
+		listDevs.sort((o1, o2) -> o1.getAge() - o2.getAge());
+
+		// java 8 only, lambda also, to print the List
+		listDevs.forEach((developer) -> System.out.println(developer));
+
+		// lambda valid, parameter type is optional
+		listDevs.sort((o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+
+		// Lambda expression to sort a List using their salary, reversed order.
+		Comparator<Developer> salaryComparator = (o1, o2) -> o1.getSalary().compareTo(o2.getSalary());
+		listDevs.sort(salaryComparator.reversed());
+
+	}
+
+	private static List<Developer> getDevelopers() {
+
+		List<Developer> result = new ArrayList<Developer>();
+
+		result.add(new Developer("mkyong", new BigDecimal("70000"), 33));
+		result.add(new Developer("alvin", new BigDecimal("80000"), 20));
+		result.add(new Developer("jason", new BigDecimal("100000"), 10));
+		result.add(new Developer("iris", new BigDecimal("170000"), 55));
+
+		return result;
+
+	}
+
+}
